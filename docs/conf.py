abe7211c647ac02037643bc57eed0f1ddc37b71c
John McGuigan <jrm5100@psu.edu>
2021-03-15 12:41:42 -0400

    Use CLARITE v1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -17,32 +17,31 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'CLARITE GUI'
-copyright = '2019, John McGuigan'
-author = 'John McGuigan'
+project = "CLARITE GUI"
+copyright = "2021, Hall Lab"
+author = "John McGuigan"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = "1.2.0"
 
 
 # -- General configuration ---------------------------------------------------
 
 # Main file
-master_doc = 'index'
+master_doc = "index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -50,16 +49,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 import sphinx_rtd_theme
+
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_logo = '_static/clarite_logo.png'
+html_logo = "_static/clarite_logo.png"
 html_theme_options = {
-    'logo_only': True,
+    "logo_only": True,
 }
